8dd10ec759706b481b0dc9cc7bcb1f76cb20677d
smartirfan <smartirfan@gmail.com>
2017-03-27 15:59:47 +0200

    Added the new project structure

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works! This title will stay here when you navigate, because it is part of the parent';
+}
---
+++ src/app/services/admin-auth-gurad.service.ts
@@ -0,0 +1,18 @@
+﻿import { Injectable } from '@angular/core';
+import {CanActivate, Router, ActivatedRouteSnapshot, Params} from "@angular/router";
+
+
+// This middleware should be used in the routes you want to guard due to access clearence.
+
+@Injectable()
+export class AdminAuthGuardService implements CanActivate{
+
+
+    canActivate(route: ActivatedRouteSnapshot){
+      // your logic to determine if the navigation request is good to go.
+      // return false or true if you want to pass or not.
+        return true;
+    }
+    constructor(private router:Router) { }
+
+}
