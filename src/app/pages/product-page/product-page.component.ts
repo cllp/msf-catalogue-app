846a5d1751c2ebc289b6bbeedecb45ae5b4085bd
shirin.danehpash@yahoo.com <shirin.danehpash@yahoo.com>
2017-03-29 11:45:04 +0200

    Updated the routing between pages

--- src/app/pages/product-page/product-page.component.ts
+++ src/app/pages/product-page/product-page.component.ts
@@ -1,10 +1,7 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
-<<<<<<< HEAD
+
 import { Location }               from '@angular/common';
-=======
-import { Location } from '@angular/common';
->>>>>>> 9a4ff85b0dcf6737a8626e901da647b2274c5588
 
 @Component({
   selector: 'app-product-page',
@@ -14,17 +11,12 @@ import { Location } from '@angular/common';
 export class ProductPageComponent implements OnInit {
 
   constructor(private router:Router,
-<<<<<<< HEAD
+
     private location: Location) { }
-=======
-  private location: Location) { }
->>>>>>> 9a4ff85b0dcf6737a8626e901da647b2274c5588
 
   ngOnInit() {
   }
-  goBack(): void {
-    this.location.back();
-  }
+
   //This is a method that you can use in the html.
   goToProducts(){
     //Notice the router instance how it was loaded in the constructor.
