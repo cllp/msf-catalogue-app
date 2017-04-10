ce829d1294338e772c89c17b95df3645aa2d61c2
Indhu <indhumathi.ramanathan@gmail.com>
2017-04-10 13:20:23 +0200

    Menu Component added to AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,22 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { DropdownModule } from "ngx-dropdown";
 
 import { AppComponent } from './app.component';
 import {RouterModule} from "@angular/router";
 import { APP_ROUTES } from "./routes/routes";
 import { ProductPageComponent } from './pages/product-page/product-page.component';
-import {Service} from "./services/service";
-import {HttpClient} from "./services/http-client.service";
+import { Service } from "./services/service";
+import { HttpClient } from "./services/http-client.service";
 import { AdminAuthGuardService } from "./services/admin-auth-gurad.service";
 import { ProductDetailsPageComponent } from './pages/product-details-page/product-details-page.component';
 import { FakeService } from "./services/fake.service";
 import { LoginPageComponent } from "./authentication/login-page/login-page.component";
 import { LoggedInPageComponent } from "./authentication/logged-page/logged-page.component";
 import { AlertModule } from 'ng2-bootstrap';
-import {AuthService} from "./services/auth.service";
+import { AuthService} from "./services/auth.service";
+import { MenuPageComponent} from "./pages/menu-page/menu-page.component";
 
 
 @NgModule({
@@ -24,14 +26,16 @@ import {AuthService} from "./services/auth.service";
     ProductPageComponent,
     ProductDetailsPageComponent,
     LoginPageComponent,
-    LoggedInPageComponent
+    LoggedInPageComponent,
+    MenuPageComponent
   ],
   imports: [
     RouterModule.forRoot(APP_ROUTES),
     AlertModule.forRoot(),
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DropdownModule
   ],
   //All services should be registered here
   providers: [
