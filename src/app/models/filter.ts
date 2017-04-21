c4b5e11b1d8db3f28cf5fdbc2cb6a3bc8824ddce
Indhu <indhumathi.ramanathan@gmail.com>
2017-04-21 11:43:13 +0200

    productFilter Changes

--- src/app/models/filter.ts
+++ src/app/models/filter.ts
@@ -1,6 +1,31 @@
-export interface IFilter { 
-    filterGroupDescription:string;
-    filterGroupId: number;
-    filter: string;
-    filterCriteria: string;
+export interface IFilterItem { 
+    Filter:string;
+    FilterCriteria:string;
+    IsChecked:boolean;
 }
+
+export interface IFilterGroup {
+    FilterGroupDescription:string;
+    FilterItemsGroup:IFilterItem[];
+}
+
+export interface IFilter { 
+    ProductCategory:string;
+    FilterGroups:IFilterGroup[];
+}
+
+/*export interface IFilter { 
+    productcategory:string;
+    filtergroups:IFilterGroup[];
+}
+
+export interface IFilterGroup {
+    filtergroupdescription: string;
+    filteritemsgroup: IFilterItem[];
+}
+
+export interface IFilterItem { 
+    filter: string;
+    filtercriteria: string;
+    ischecked: boolean;
+}*/
--- src/app/services/productFilter.service.ts
+++ src/app/services/productFilter.service.ts
@@ -3,24 +3,29 @@ import {Service} from "./service";
 import {HttpClient} from "./http-client.service";
 import {Observable} from "rxjs/Rx";
 import {IFilter} from "../models/filter";
+import {IFilterGroup} from "../models/filter";
+import {IFilterItem} from "../models/filter";
 import {IProduct} from "../pages/products";
 
 const _FIL_BASE = 'http://localhost:4200/src/api/filter.json';
 
 @Injectable()
-export class ProductFilterService extends Service{
-posts:IFilter[]
+export class ProductFilterService extends Service {
 private url = _FIL_BASE;
+private url2 = 'http://localhost:4200/src/api/product2.json';
     constructor(private http:HttpClient) {super(); }
-    GetAllFilters():Observable<IFilter[]> {
+    GetAllFilters():Observable<IFilterGroup[]> {
         return this.http.get(this.url)
             .map(this.extractData)
             .do(data => console.log('All: ' +  JSON.stringify(data)))
             .catch(this.handleError);
     }
 
-    GetFilterCriteria(filterCriteria: any) {
-        //request()
-        alert("Hello"+ filterCriteria);
+    GetFilterCriteria(ProductCategory, FilterGroups):Observable<IProduct[]> {
+       //alert(FilterGroups); 
+       return this.http.get(this.url2)
+            .map(this.extractData)
+            .do(data => console.log('All: ' +  JSON.stringify(data)))
+            .catch(this.handleError);
     }
 }
